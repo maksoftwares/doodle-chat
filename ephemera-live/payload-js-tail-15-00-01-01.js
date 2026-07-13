b49e0981e5a5152dd4ac6aa9e51d4b1f6806740a
MUHAMMAD ALI KHAN <51024339+maksoftwares@users.noreply.github.com>
2026-07-13 11:47:51 +0400

    Add Ephemera live tail payload 15a2b

---
+++ ephemera-live/payload-js-tail-15-00-01-01.js
@@ -0,0 +1,1 @@
+window.__EPH_JS=window.__EPH_JS||[];window.__EPH_JS.push("7WGNx3vukmVJNt3+MaRIGXJ3lpTdL14d435mmc2sTb/p3TH3YD/eYtcG4t9mCuJ0HrsSVhT2Ym8lslsiqEyzyVBXlHwiUnhRdDzW7rtR0h1NmzZ3/ZJneWweluFmP");
